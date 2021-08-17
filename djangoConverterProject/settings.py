87fac88570cc30b17a3a2699e5334393fef26e4b
elijahpraise <elijahpraise21@gmail.com>
2021-08-17 17:04:16 +0200

    code

--- djangoConverterProject/settings.py
+++ djangoConverterProject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-4p6l5k4!6_ov%23#m7z%&h7ic2k4^1tcb+_79ozmeb$3wcd!$5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', "immense-savannah-14817.herokuapp.com"]
+ALLOWED_HOSTS = ['127.0.0.1', ".herokuapp.com"]
 
 # Application definition
 
