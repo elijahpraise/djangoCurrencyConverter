55404cb49d4799f75c137ac6a40353952009cffa
elijahpraise <elijahpraise21@gmail.com>
2021-08-17 16:24:35 +0200

    made changes to settings and runtime.txt

--- djangoConverterProject/settings.py
+++ djangoConverterProject/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4p6l5k4!6_ov%23#m7z%&h7ic2k4^1tcb+_79ozmeb$3wcd!$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 # Application definition
 
