0052fb8a3ce3021e0b1fb9e4b71099f2b24f75ce
elijahpraise <elijahpraise21@gmail.com>
2021-08-18 11:59:02 +0200

    code

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from converter.functions import rate_display, symbols_display
-from  import CurrencySymbols
+from currency_symbols import CurrencySymbols
 
 rates = rate_display('https://api.exchangerate.host/latest')
 symbols = symbols_display(rates)
